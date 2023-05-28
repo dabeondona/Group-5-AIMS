4776898206004cd32d53c23e6b1c0806b97cf823
ondon <ondon@Dabee>
2023-05-28 12:03:41 +0800

    Merge origin/master-Ondona into master-Ondona
    Conflicts:
    	src/main/java/com/mycompany/japps/AdminNewsletter.java
    	src/main/java/com/mycompany/japps/AdminTopPanelButton.java

--- src/main/java/com/mycompany/japps/AdminNewsletter.java
+++ src/main/java/com/mycompany/japps/AdminNewsletter.java
@@ -26,21 +26,4 @@ public class AdminNewsletter extends JPanel {
     }
 }
 
-package com.mycompany.japps;
-import javax.swing.*;
-import java.awt.event.*;
-import java.awt.*;
-import javax.swing.border.*;
 
-
-public class AdminNewsletter extends JPanel {
-    
-    public AdminNewsletter(JPanel cardPanel, CardLayout cardLayout) {
-        this.setLayout(new BorderLayout());
-        
-        this.add(new AdminTopPanelButton(cardPanel, cardLayout), BorderLayout.NORTH);
-       
-        this.setSize(Japps.getGUIWidth(),Japps.getGUIHeight());
-        this.setVisible(true);
-    }
-}
